ac5302e77fb1049942ce8dfe8f08b7594d434a7a
Guilherme Zaparoli Gomes <112036882+guilhermezaparoli@users.noreply.github.com>
2023-05-03 13:35:31 -0300

    initial commit

---
+++ src/Items.js
@@ -0,0 +1,46 @@
+const items = [
+  {
+    id: 1,
+    name: "BBQ",
+    desc: "Molho barbecue com frango grelhado temperado",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.chicken-lovers.b954bbe4c3fed2d6e1bdb2953902be9e.1.jpg?width=300",
+    price: 12.5,
+  },
+  {
+    id: 2,
+    name: "Suprema",
+    desc: "Salsicha italiana, presunto, azeitona preta",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.super-supreme.21f3260bdd33813f550cd523fbd8d073.1.jpg?width=300",
+    price: 14.5,
+  },
+  {
+    id: 3,
+    name: "Canadense",
+    desc: "Bacon, presunto canadense",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.canadian.67c22dc316f702bf4d992c667d22fbf0.1.jpg?width=300",
+    price: 12.5,
+  },
+  {
+    id: 4,
+    name: "Vegana",
+    desc: "Cogumelos fatiados, pimentão verde e cebola roxa",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.veggie-lovers.48ba54d49ff2257671521b477967633a.1.jpg?width=300",
+    price: 11.5,
+  },
+  {
+    id: 5,
+    name: "Jalapeno",
+    desc: "Salsicha italiana, calabresa e pimenta jalapenõ",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.italian-classic.ad7765e7d0bfed6e00b09053416cba63.1.jpg?width=300",
+    price: 14.5,
+  },
+  {
+    id: 6,
+    name: "Pepperoni",
+    desc: "Calabresa dupla e mussarela extra.",
+    img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.pepperoni-lovers.69f7bdf7b6f50a87eb2886934fe0be9f.1.jpg?width=300",
+    price: 9.99,
+  },
+];
+
+export default items;
